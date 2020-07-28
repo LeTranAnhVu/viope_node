df380d8dcaf1116588eba3275a4de54d8d52438b
letrananhvu <letrananhvu>
2020-07-28 16:07:39 +0300

    init commit

---
+++ db/customer.js
@@ -0,0 +1,97 @@
+const db = require('./config');
+
+// fetch all
+const fetchAll = (req, res) => {
+  db.query('SELECT * FROM customers', (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'there are some errors'});
+    } else {
+      res.json(result.rows);
+    }
+  });
+
+};
+
+// fetch by id
+const fetchById = (req, res) => {
+  db.query(`SELECT * FROM customers WHERE id=${req.params.id}`, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'there are some errors'});
+    } else {
+      if (result.rows[0]) {
+        res.json(result.rows[0]);
+      } else {
+        res.status(404).json({'message': 'NOT FOUND'});
+      }
+    }
+  });
+};
+
+// create by id
+const addNew = (req, res) => {
+  const {firstname, lastname, email, phone} = req.body;
+  const query = {
+    text: 'INSERT INTO customers (firstname, lastname, email, phone) VALUES ($1, $2, $3, $4)',
+    values: [firstname, lastname, email, phone]
+  };
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'there are some errors'});
+    } else {
+      console.log(result);
+      let newCustomer = {firstname, lastname, email, phone};
+      res.status(200).json(newCustomer);
+    }
+  });
+};
+
+// delete by id
+const deleteById = (req, res) => {
+  db.query(`DELETE FROM customers WHERE id=${req.params.id}`, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'there are some errors'});
+    } else {
+      console.log(result);
+      if (result.rowCount) {
+        res.status(204).json({'message': 'delete success'});
+      } else {
+        res.status(404).json({'message': 'NOT FOUND'});
+      }
+    }
+  });
+};
+
+// put by id
+const updateById = (req, res) => {
+  const {firstname, lastname, email, phone} = req.body;
+  const query = {
+    text: 'UPDATE customers SET firstname=$1, lastname=$2, email=$3, phone=$4 WHERE id = $5',
+    values: [firstname, lastname, email, phone, req.params.id]
+  };
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({message: 'there are some errors'});
+    } else {
+      console.log(result);
+      if (result.rowCount) {
+        let updatedCustomer = {firstname, lastname, email, phone};
+        res.status(200).json(updatedCustomer);
+      } else {
+        res.status(404).json({'message': 'NOT FOUND'});
+      }
+    }
+  });
+};
+
+module.exports = {
+  fetchAll,
+  addNew,
+  fetchById,
+  deleteById,
+  updateById
+};
---
+++ server.js
@@ -0,0 +1,37 @@
+// file structure
+// server.js <--- this file
+// db/
+//   config.js
+//   customer.js
+// node_modules
+// package.json
+
+const express = require('express');
+const db = require('./db/config');
+
+const queryCustomer = require('./db/customer');
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+
+// fetch all
+app.get('/api/customers', queryCustomer.fetchAll);
+
+// fetch by id
+app.get('/api/customers/:id', queryCustomer.fetchById);
+
+// create by id
+app.post('/api/customers', queryCustomer.addNew);
+
+// delete by id
+app.delete('/api/customers/:id', queryCustomer.deleteById);
+
+// put by id
+app.put('/api/customers/:id', queryCustomer.updateById);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
